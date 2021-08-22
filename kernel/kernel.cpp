a8c36a27ce7d8eba67ba4abb27a0dad82fdc91a8
ArtichOwO <leo.grange@icloud.com>
2021-08-22 09:36:16 +0200

    [kernel] Use new `screen` functions

--- kernel/kernel.cpp
+++ kernel/kernel.cpp
@@ -1,12 +1,16 @@
-void main() {
-    /* Assuming we're on a colored screen
-     * will add this -> https://wiki.osdev.org/Detecting_Colour_and_Monochrome_Monitors
-     */
-    char * video_memory = (char *) 0xb8000;
-    char * msg = (char *)"Hello, World!";
+#include "../drivers/screen.h"
 
-    for (char c = *msg; c; c=*++msg) {
-        *video_memory = c;
-        video_memory++;video_memory++;
-    }
+void main() {
+    clear_screen();
+
+    char * msg = (char *) "Hello, World! Welcome to \n\n"
+                "  ___       _   _      _     _____ _____ \n"
+                " / _ \\     | | (_)    | |   |  _  /  ___|\n"
+                "/ /_\\ \\_ __| |_ _  ___| |__ | | | \\ `--. \n"
+                "|  _  | '__| __| |/ __| '_ \\| | | |`--. \\\n"
+                "| | | | |  | |_| | (__| | | \\ \\_/ /\\__/ /\n"
+                "\\_| |_/_|   \\__|_|\\___|_| |_|\\___/\\____/ \n\n"
+                "Please wait, there will be soon more things to do here ;3";
+
+    kprint(msg, 0x02);
 }
