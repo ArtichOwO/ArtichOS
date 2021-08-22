047167ba7d5d573f7b4903b853d5c68057a670bc
ArtichOwO <leo.grange@icloud.com>
2021-08-22 09:53:10 +0200

    [stdlib] String utilities

---
+++ stdlib/string.h
@@ -0,0 +1,13 @@
+#ifndef STD_STRINGS_H
+#define STD_STRINGS_H
+
+namespace std {
+	void int_to_ascii(int n, char str[]);
+	void reverse(char s[]);
+	int strlen(char s[]);
+	void backspace(char s[]);
+	void append(char s[], char n);
+	int strcmp(char s1[], char s2[]);
+}
+
+#endif // STD_STRING_H
