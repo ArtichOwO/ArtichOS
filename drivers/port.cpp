6638090bc82e6a32a8f13909d906cabcace8947a
ArtichOwO <leo.grange@icloud.com>
2021-08-26 21:11:19 +0200

    [drivers] Cleaning

--- drivers/port.cpp
+++ drivers/port.cpp
@@ -1,10 +1,10 @@
 /*
- * Read a byte from the specified port
+ * Read/send a byte from the specified port
  */
 
 #include "port.h"
 
-unsigned char port_byte_in (unsigned short port) {
+unsigned char port_byte_in(unsigned short port) {
     unsigned char result;
     __asm__("in %%dx, %%al" 
             : "=a" (result) 
@@ -12,13 +12,13 @@ unsigned char port_byte_in (unsigned short port) {
     return result;
 }
 
-void port_byte_out (unsigned short port, unsigned char data) {
+void port_byte_out(unsigned short port, unsigned char data) {
     __asm__("out %%al, %%dx" 
             : 
             : "a" (data), "d" (port));
 }
 
-unsigned short port_word_in (unsigned short port) {
+unsigned short port_word_in(unsigned short port) {
     unsigned short result;
     __asm__("in %%dx, %%ax" 
             : "=a" (result) 
@@ -26,7 +26,7 @@ unsigned short port_word_in (unsigned short port) {
     return result;
 }
 
-void port_word_out (unsigned short port, unsigned short data) {
+void port_word_out(unsigned short port, unsigned short data) {
     __asm__("out %%ax, %%dx" 
             : 
             : "a" (data), "d" (port));
