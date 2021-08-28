8d4f5037d3936927ff4a8515ffa4fbadf0a819f4
ArtichOwO <leo.grange@icloud.com>
2021-08-28 11:16:22 +0200

    [stdlib/string] Cleaning

--- stdlib/string.cpp
+++ stdlib/string.cpp
@@ -1,64 +1,64 @@
 #include "string.h"
 
 namespace std {
-    void itoa(int n, char str[]) {
-        int i, sign;
-        if ((sign = n) < 0) n = -n;
-        i = 0;
-        do {
-            str[i++] = n % 10 + '0';
-        } while ((n /= 10) > 0);
+  void itoa(int n, char str[]) {
+    int i, sign;
+    if ((sign = n) < 0) n = -n;
+    i = 0;
+    do {
+      str[i++] = n % 10 + '0';
+    } while ((n /= 10) > 0);
 
-        if (sign < 0) str[i++] = '-';
-        str[i] = '\0';
+    if (sign < 0) str[i++] = '-';
+    str[i] = '\0';
 
-        reverse(str);
+    reverse(str);
+  }
+
+  void itox(unsigned int i, char * str) {
+    unsigned char n;
+    
+    str += 4;
+    *str = '\0';
+    
+    for (n = 4; n != 0; --n) {
+      *--str = "0123456789ABCDEF"[i & 0x0F];
+      i >>= 4;
     }
+  }
 
-    void itox(unsigned int i, char * str) {
-        unsigned char n;
-     
-        str += 4;
-        *str = '\0';
-     
-        for (n = 4; n != 0; --n) {
-            *--str = "0123456789ABCDEF"[i & 0x0F];
-            i >>= 4;
-        }
+  void reverse(char s[]) {
+    int c, i, j;
+    for (i = 0, j = strlen(s)-1; i < j; i++, j--) {
+      c = s[i];
+      s[i] = s[j];
+      s[j] = c;
     }
+  }
 
-    void reverse(char s[]) {
-        int c, i, j;
-        for (i = 0, j = strlen(s)-1; i < j; i++, j--) {
-            c = s[i];
-            s[i] = s[j];
-            s[j] = c;
-        }
-    }
+  int strlen(char s[]) {
+    int i = 0;
+    while (s[i] != '\0') ++i;
+    return i;
+  }
 
-    int strlen(char s[]) {
-        int i = 0;
-        while (s[i] != '\0') ++i;
-        return i;
-    }
+  void append(char s[], char n) {
+    int len = strlen(s);
+    s[len] = n;
+    s[len+1] = '\0';
+  }
 
-    void append(char s[], char n) {
-        int len = strlen(s);
-        s[len] = n;
-        s[len+1] = '\0';
-    }
-
-    void backspace(char s[]) {
-        int len = strlen(s);
-        s[len-1] = '\0';
-    }
+  void backspace(char s[]) {
+    int len = strlen(s);
+    s[len-1] = '\0';
+  }
 
     /* Returns <0 if s1<s2, 0 if s1==s2, >0 if s1>s2 */
-    int strcmp(char s1[], char s2[]) {
-        int i;
-        for (i = 0; s1[i] == s2[i]; i++) {
-            if (s1[i] == '\0') return 0;
-        }
-        return s1[i] - s2[i];
+  int strcmp(char s1[], char s2[]) {
+    int i;
+    for (i = 0; s1[i] == s2[i]; i++) {
+      if (s1[i] == '\0') return 0;
     }
+    return s1[i] - s2[i];
+  }
 }
