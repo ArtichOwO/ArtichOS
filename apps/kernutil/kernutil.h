b44fc1850a772531a57e24726bd1e5bcaec94d71
ArtichautCosmique <leo.grange@icloud.com>
2021-10-29 16:48:32 +0200

    init: Redoing everything from zero :D
    Also following this https://www.conventionalcommits.org/en/v1.0.0/#specification more or less

--- apps/kernutil/kernutil.h
+++
@@ -1,9 +0,0 @@
-#ifndef APP_KERNUTIL_H
-#define APP_KERNUTIL_H
-
-namespace kernutilapp {
-int shutdown();
-int reboot();
-}
-
-#endif // APP_KERNUTIL_H
--- apps/shell/shell.h
+++
@@ -1,8 +0,0 @@
-#ifndef APP_SHELL_H
-#define APP_SHELL_H
-
-namespace shellapp {
-	int main(int argc, char * argv[]);
-}
-
-#endif // APP_SHELL_H
