1e016adb233eac4c1add1132e0397881213b81e4
ArtichautCosmique <leo.grange@icloud.com>
2021-11-01 22:44:04 +0100

    Revert "fix(libc): Now they're pointers"
    This reverts commit 894da73de0bf3dee3afc8f80c67aae85b1016f7c.
    useless commit sorry shame on me

--- libc/stdint.h
+++ libc/stdint.h
@@ -7,7 +7,7 @@ typedef unsigned short uint16_t;
 typedef signed char int8_t;
 typedef signed short int16_t;
 
-typedef unsigned short * uintptr_t;
-typedef signed short * intptr_t;
+typedef unsigned short uintptr_t;
+typedef signed short intptr_t;
 
 #endif // LIBC_STDINT_H
