f371e63dbfd3b3594f3ebb75ffc0fbf1a70ae988
ArtichautCosmique <leo.grange@icloud.com>
2021-11-06 20:52:32 +0100

    fix(boot): Separate files for `print` and `print_hex`

---
+++ boot/print.asm
@@ -0,0 +1,19 @@
+[bits 16]
+
+section .boot.text
+
+print:
+  pusha
+	mov bp, sp
+	cont:
+	lodsb
+	or al, al
+	jz dne
+	mov ah, 0x0e
+	mov bx, 0
+	int 10h
+	jmp cont
+	dne:
+	mov sp, bp
+	popa
+	ret
