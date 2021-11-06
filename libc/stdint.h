89c611713de821b4f3fd80367a27897d2e4834c3
ArtichautCosmique <leo.grange@icloud.com>
2021-11-06 23:32:50 +0100

    feat(libc): Add `uint32_t` and `int32_t`

--- libc/stdint.h
+++ libc/stdint.h
@@ -3,9 +3,11 @@
 
 typedef unsigned char uint8_t;
 typedef unsigned short uint16_t;
+typedef unsigned long uint32_t;
 
 typedef signed char int8_t;
 typedef signed short int16_t;
+typedef signed long int32_t;
 
 typedef unsigned short uintptr_t;
 typedef signed short intptr_t;
