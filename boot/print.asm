9a8bcc436d476ea773c3f30c8bad946628e5d716
ArtichautCosmique <leo.grange@icloud.com>
2021-11-08 22:08:08 +0100

    fix(boot): Tabs to space

--- boot/print.asm
+++ boot/print.asm
@@ -4,16 +4,16 @@ section .boot.text
 
 print:
   pusha
-	mov bp, sp
-	cont:
-	lodsb
-	or al, al
-	jz dne
-	mov ah, 0x0e
-	mov bx, 0
-	int 10h
-	jmp cont
-	dne:
-	mov sp, bp
-	popa
-	ret
+  mov bp, sp
+  cont:
+  lodsb
+  or al, al
+  jz dne
+  mov ah, 0x0e
+  mov bx, 0
+  int 10h
+  jmp cont
+  dne:
+  mov sp, bp
+  popa
+  ret
