6dcec1a78f30773212b72fbd3ff0ba70b4a43443
ArtichautCosmique <leo.grange@icloud.com>
2021-12-10 02:17:18 +0100

    feat: Serial on COM1

--- Makefile
+++ Makefile
@@ -19,7 +19,7 @@ clean:
 
 run: ArtichOS.bin
 	qemu-system-i386 -drive format=raw,file=$(BUILD_DIR)/$<,index=0,if=floppy \
-	                 -monitor stdio -serial file:output/serial.log
+	                 -serial mon:stdio
 
 all: clean run
 
--- kernel/boot/boot.asm
+++ kernel/boot/boot.asm
@@ -2,6 +2,7 @@ kernel_offset equ 0x500
 sector_quantity equ 59
 
 GLOBAL _start
+EXTERN init_serial, write_serial_string
 
 BITS 16
 
@@ -26,11 +27,25 @@ _start:
     mov ah, 0
     int 0x10
 
+    call init_serial
+    cmp ax, 0
+    jnz _start.no_serial
+
+    push word welcome_string
+    call write_serial_string
+    add sp, 2
+
     mov ah, 0
     int 0x16
+
+    _start.no_serial:
 
     jmp $
 
 SECTION .boot.data
 
 boot_drive db 0x00
+
+SECTION .boot.rodata
+
+welcome_string db "Hello, World! on ArtichOS",0x0A,0x0D,0
