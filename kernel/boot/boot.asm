85317a1673c65a577876f93b117338391c898fc7
ArtichautCosmique <leo.grange@icloud.com>
2021-12-10 18:48:57 +0100

    feat: Kernel loading

--- kernel/boot/boot.asm
+++ kernel/boot/boot.asm
@@ -1,14 +1,15 @@
-kernel_offset equ 0x500
-sector_quantity equ 59
-
-GLOBAL _start
-EXTERN init_serial, write_serial_string
-
 BITS 16
+
+GLOBAL _start, boot_drive, newline_string
+EXTERN init_serial, write_serial_string, \
+       load_kernel, kernel_offset
 
 SECTION .boot.text
 
 _start:
+    push bp
+    mov bp, sp
+
     mov [boot_drive], dl
 
     cli
@@ -23,10 +24,6 @@ _start:
     sti
     cld
 
-    mov al, 03h
-    mov ah, 0
-    int 0x10
-
     call init_serial
     cmp ax, 0
     jnz _start.no_serial
@@ -35,11 +32,13 @@ _start:
     call write_serial_string
     add sp, 2
 
-    mov ah, 0
-    int 0x16
+    jmp _start.load_kernel
 
     _start.no_serial:
+    jmp $
 
+    _start.load_kernel:
+    call load_kernel
     jmp $
 
 SECTION .boot.data
@@ -48,4 +47,5 @@ boot_drive db 0x00
 
 SECTION .boot.rodata
 
-welcome_string db "Hello, World! on ArtichOS",0x0A,0x0D,0
+welcome_string db 0x0A,0x0D,0x1b,"[31m","Booting...",0x1b,"[0m",0x0A,0x0D,0
+newline_string db 0x0A,0x0D,0
---
+++ kernel/boot/load_kernel.asm
@@ -0,0 +1,77 @@
+BITS 16
+
+kernel_offset equ 0x500
+sector_quantity equ 59
+
+GLOBAL load_kernel, kernel_offset
+EXTERN boot_drive, write_serial_string, \
+	   newline_string
+
+SECTION .boot.text
+
+print_disk_type:
+	push bp
+	mov bp, sp
+
+	print_disk_type.floppy:
+	cmp byte [boot_drive], 0
+	jne print_disk_type.disk
+	push word floppy
+	jmp print_disk_type.end
+	print_disk_type.disk:
+	cmp byte [boot_drive], 0x80
+	jne print_disk_type.unknown_disk
+	push word disk
+	jmp print_disk_type.end
+	print_disk_type.unknown_disk:
+	push word unknown_disk
+
+	print_disk_type.end:
+	push word disk_string
+	call write_serial_string
+	add sp, 2
+	call write_serial_string
+	add sp, 2
+	push word newline_string
+    call write_serial_string
+    add sp, 2
+
+	pop bp
+	ret
+
+load_kernel:
+	push bp
+	mov bp, sp
+
+	call print_disk_type
+
+	mov dl, [boot_drive]
+	mov ah, 02h
+	mov al, sector_quantity
+	mov cx, 0002h
+	xor dh, dh
+	mov bx, kernel_offset
+
+	int 13h
+	jnc load_kernel.end
+
+	push word disk_error_string
+	call write_serial_string
+	add sp, 2
+	jmp $
+
+	load_kernel.end:
+	pop bp
+	ret
+
+SECTION .boot.data
+
+test_error db 0x00,0x00
+
+SECTION .boot.rodata
+
+floppy db "FLP",0
+disk db "IDE",0
+unknown_disk db "???",0
+disk_string db "DSK: ",0
+disk_error_string db "DSK ERR",0
