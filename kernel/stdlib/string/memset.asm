69b8b440e66ba2bff5daad85746f756f655b6a7f
ArtichautCosmique <leo.grange@icloud.com>
2021-12-11 01:10:16 +0100

    feat(stdlib): `memset`

---
+++ kernel/stdlib/string/memset.asm
@@ -0,0 +1,36 @@
+BITS 16
+
+GLOBAL memset
+
+SECTION .kernel.text
+
+memset:
+	; 0: Buffer offset
+	; 1: Buffer segment
+	; 2: Value
+	; 3: Size
+	; Ret: Buffer offset
+	push bp
+	mov bp, sp
+
+	xor cx, cx
+	mov byte dl, [bp+8]
+
+	mov ax, [bp+6]
+	mov es, ax
+
+	memset.for:
+	cmp cx, [bp+10]
+	je memset.end
+
+	mov di, [bp+4]
+	add di, cx
+	mov [es:di], dl
+	inc cx
+
+	jmp memset.for
+
+	memset.end:
+	mov ax, [bp+4]
+	pop bp
+	ret
