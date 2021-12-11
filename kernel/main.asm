b7a3831c79b10f0ff789f3f4cb2394258bf9d1c0
ArtichautCosmique <leo.grange@icloud.com>
2021-12-11 01:24:05 +0100

    fix: Spaces in `kmain` too

--- kernel/main.asm
+++ kernel/main.asm
@@ -8,21 +8,22 @@ EXTERN write_serial_string
 SECTION .kernel.text
 
 kmain:
-	push bp
-	mov bp, sp
+    push bp
+    mov bp, sp
 
-	push word welcome_string
-	call write_serial_string
-	add sp, 2
+    push word welcome_string
+    call write_serial_string
+    add sp, 2
 
-	pop bp
-	jmp $
+    pop bp
+    jmp $
 
 SECTION .kernel.rodata
 
 welcome_string db newline, \
-	"+--------------+",newline, \
-	"|",cyan_text,"Kernel loaded!",reset_color_text,"|",newline, \
-	"+--------------+",newline, \
-	"| ",yellow_text,"Welcome! :3",reset_color_text,"  |",newline, \
-	"+--------------+",0
+    "+--------------+",newline, \
+    "|",cyan_text,"Kernel loaded!",reset_color_text,"|",newline, \
+    "+--------------+",newline, \
+    "| ",yellow_text,"Welcome! :3",reset_color_text,"  |",newline, \
+    "+--------------+",0
+
